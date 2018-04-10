9339124e468bfd524c85016eb7565be0c2a70622
Stefan Falk <stefan.falk@ct-systeme.com>
2018-04-10 18:32:29 +0200

    Included samples for IRQ inputs

---
+++ source/AbstractIO.Samples/Sample03ButtonControlsLampEventBased.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace AbstractIO.Samples
+{
+    /// <summary>
+    /// Controls a lamp using a button without polling but using events (stemming from IRQs).
+    /// </summary>
+    public static class Sample03ButtonControlsLampEventBased
+    {
+        /// <summary>
+        /// The lamp to be controlled.
+        /// </summary>
+        private static IBooleanOutput _lamp;
+
+        public static void Run(IObservableBooleanInput button, IBooleanOutput lamp)
+        {
+            // Check arguments:
+            if (button == null) throw new ArgumentNullException(nameof(button));
+            if (lamp == null) throw new ArgumentNullException(nameof(lamp));
+
+            // Store the lamp so that the event handler can use it:
+            _lamp = lamp;
+
+            // Attach the event handler to the event that the button raises whenever it's Value changed:
+            button.ValueChanged += ButtonValueChangedHandler;
+
+            // Just wait and let the event handler react on button changes:
+            for (; ; ) Thread.Sleep(1000);
+        }
+
+        /// <summary>
+        /// Handles the <see cref="IObservableBooleanInput.ValueChanged"/> event.
+        /// </summary>
+        /// <param name="sender">The object which raised the event (that is, the button).</param>
+        /// <param name="newValue">The new value to which the button's <see cref="IBooleanInput.Value"/> property
+        /// changed.</param>
+        private static void ButtonValueChangedHandler(object sender, bool newValue)
+        {
+            // Turn the lamp on or off if the button was pressed or released, respectively:
+            _lamp.Value = newValue;
+        }
+    }
+}
--- source/AbstractIO/InputConverters.cs
+++ source/AbstractIO/InputConverters.cs
@@ -32,6 +32,66 @@ namespace AbstractIO
         }
     }
 
+    /// <summary>
+    /// A class inverting an <see cref="IObservableBooleanInput"/>.
+    /// </summary>
+    public class ObserverableBooleanInputInverter : IObservableBooleanInput
+    {
+        private readonly IObservableBooleanInput _source;
+
+        /// <summary>
+        /// This event gets fired when the Value property of the abstract input/output interfaces has changed.
+        /// </summary>
+        /// <remarks>
+        /// In addition to the Value property of the observed object, the new value to which that property changed will
+        /// be readily passed to the newValue parameter of the event handler. Thus you have the guarantee to see the
+        /// original value causing the event, not a possibly meanwhile again changed Value property. So, handlers of
+        /// this event should usually inspect their newValue parameter and not query the object's Value property.
+        /// </remarks>
+        public event BooleanValueChangedHandler ValueChanged;
+
+        /// <summary>
+        /// Raises the <see cref="ValueChanged"/> event.
+        /// </summary>
+        /// <param name="newValue">The new value to which the input has changed.</param>
+        protected void OnValueChanged(bool newValue)
+        {
+            ValueChanged(this, newValue);
+        }
+
+        /// <summary>
+        /// Creates an instance.
+        /// </summary>
+        /// <param name="source">The input to be converted.</param>
+        public ObserverableBooleanInputInverter(IObservableBooleanInput source)
+        {
+            _source = source ?? throw new ArgumentNullException(nameof(source));
+            _source.ValueChanged += SourceValueChangedHandler;
+        }
+
+        /// <summary>
+        /// Gets the inverted value of the source input passed to the constructor.
+        /// </summary>
+        public bool Value
+        {
+            get
+            {
+                return !_source.Value;
+            }
+        }
+
+        /// <summary>
+        /// Handles the _source <see cref="IObservableBooleanInput.ValueChanged"/> event.
+        /// </summary>
+        /// <param name="sender">The object raising the event (that is, <see cref="_source"/>).</param>
+        /// <param name="newValue">The new value to which <see cref="_source"/> has changed.</param>
+        private void SourceValueChangedHandler(object sender, bool newValue)
+        {
+            // Raise the event with the inverted value.
+            OnValueChanged(!newValue);
+        }
+    }
+
     #endregion
 
     #region Extension methods
@@ -53,6 +113,19 @@ namespace AbstractIO
         {
             return new BooleanInputInverter(source);
         }
+
+        /// <summary>
+        /// Creates a <see cref="IObservableBooleanInput"/> using the specified source input.
+        /// </summary>
+        /// <param name="source">The input which shall be inverted.</param>
+        /// <returns>The inverted input.</returns>
+        /// <remarks>For instance, if you have an <see cref="IBooleanInput"/> object named "input", you can just code
+        /// input.Invert() to get an inverted version of input.</remarks>
+        public static IObservableBooleanInput Invert(this IObservableBooleanInput source)
+        {
+            return new ObserverableBooleanInputInverter(source);
+        }
+
     }
 
     #endregion
